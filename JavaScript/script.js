2433d4931b3b3e8d4d7017560c33c1b09b2235ba
Aditya Sharma <110447635+adi-sharma999@users.noreply.github.com>
2023-09-20 22:20:09 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+    var swiper = new Swiper(".certi-slider", {
+      spaceBetween: 1,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop:true,
+    });
