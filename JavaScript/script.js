6700a0defb8efa9fbe571e5da71553b2eae87a42
Aditya Sharma <110447635+adi-sharma999@users.noreply.github.com>
2023-09-25 03:26:59 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,28 @@
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 250;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+  });
+}
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+})
+
+
 var swiper = new Swiper(".certi-slider",{
     spaceBetween: 1,
     centeredSlides: true,
@@ -15,3 +40,6 @@ var swiper = new Swiper(".certi-slider",{
     },
     loop:true,
 });
+
+
+
